a292a9a586f7edbb9bf2bd5a2402b3c62bcb3793
Volodymyr Gogunsky <vgogunsky@apple.com>
2025-11-30 20:16:21 +0200

    Clean up

--- opencart/opencart-docker/restart.sh
+++
@@ -1,5 +0,0 @@
-docker-compose down -v --remove-orphans
-rm -rf ./opencart_data  # Cleaning up previous host folders
-docker-compose up --build -d
-sleep 10
-docker ps
--- scripts/cleanup_sqlite_opencart.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-DB_PATH="${1:-${OPENCART_DB:-/workspace/opencart-docker/opencart.db}}"
-
-python3 "$(dirname "$0")/cleanup_sqlite_opencart.py" --db "$DB_PATH"
